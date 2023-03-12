2a7f21b6506346be9fea95392b1d805511b307b1
Gold3nEagle <g3agle@gmail.com>
2023-03-12 15:18:21 +0300

    first commit

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1557086.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 105623551, 105623556, 105623546, 105623535, 105623539, 105623538, 105623540, 105623554, 105623548, 105623549, 105623555, 105624569, 105878597, 105910422, 105916350, 106399762, 106399763, 106399764, 106399765, 106399766, 106422942 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
